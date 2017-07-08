0df405183d425e9b0baa0ed107967cf14a7e2aa6
Dominik Przywara <dominik.przywara@gmail.com>
2017-07-08 11:12:59 +0200

    Repository added

---
+++ src/Fibon.Api/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fibon.Api.Repository
+{
+    public interface IRepository
+    {
+        void Insert(int number, int value);
+        int? Get(int number);
+    }
+}
---
+++ src/Fibon.Api/Repository/RepositoryInMemory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Fibon.Api.Repository
+{
+    public class RepositoryInMemory : IRepository
+    {
+        private readonly Dictionary<int, int> _storage = new Dictionary<int, int>();
+
+        public void Insert(int number, int value)
+        {
+            _storage.Add(number, value);
+        }
+
+        public int? Get(int number)
+        {
+            int value;
+            if (_storage.TryGetValue(number, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+
+
+    }
+}
