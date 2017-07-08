0d3b4fc34e3104a53c8da82fd091c77607e6a216
Dominik Przywara <dominik.przywara@gmail.com>
2017-07-08 10:11:23 +0200

    Fibonacci controller

---
+++ src/Fibon.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController : Controller
+    {
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            return Content(0.ToString());
+        }
+
+        [HttpPost("{number}")]
+        public IActionResult Post(int number)
+        {
+            return Accepted($"fibonacci/{number}", null);
+        }
+    }
+}
