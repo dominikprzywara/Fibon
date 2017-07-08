cea7c0adde58c9a68d4b01054a9e0c2d7626f5aa
Dominik Przywara <dominik.przywara@gmail.com>
2017-07-08 11:55:13 +0200

    Adding calculated value to event

---
+++ src/Fibon.Api/Handlers/ValueCalculatedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Fibon.Api.Repository;
+using Fibon.Messages.Events;
+using RawRabbit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fibon.Api.Handlers
+{
+    public class ValueCalculatedEventHandler : IEventHandler<ValueCalculatedEvent>
+    {
+        private readonly IRepository _repository;
+
+        public ValueCalculatedEventHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task HandleAsync(ValueCalculatedEvent @event)
+        {
+            _repository.Insert(@event.Number, @event.Value);
+            await Task.CompletedTask;
+        }
+    }
+}
